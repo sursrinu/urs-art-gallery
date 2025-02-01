859d67258aa3bbc69458ff9341a3278f594a3205
sursrinu <46637009+sursrinu@users.noreply.github.com>
2025-02-01 17:47:01 -0600

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/your-repository-name/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/urs-art-gallery/' : '/'
 };
